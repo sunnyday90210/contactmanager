856a14939d92bf9e1e99c3a42c80d8581771b15f
Robert Wollny <robertjohnwollny@gmail.com>
2018-10-25 01:58:33 -0700

    added 2 components

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact
+          name="Mahshid Mianchi"
+          email="mm@gmail.com"
+          phone="777-777-7777"
+        />
       </div>
     );
   }
