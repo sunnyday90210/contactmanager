42d38b60f19d9d1de06a80fe4d259eeb9fd5f0af
Robert Wollny <robertjohnwollny@gmail.com>
2018-10-25 00:31:23 -0700

    started new react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Contact />
+      </div>
+    );
+  }
+}
+
+export default App;
